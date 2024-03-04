ebe4d851b4f6fefa0d3630ad6b36098aa3af618d
Ilya Pankratov <i.pankratov.main@gmail.com>
2024-03-04 21:07:43 +0300

    Create stdlib
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

---
+++ JaML/lib/stdlib.c
@@ -0,0 +1,22 @@
+/*
+    Copyright 2024, Ilya Pankratov, Maxim Drumov
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+uint64_t print_int(uint64_t n)
+{
+    printf("%" PRIu64 "\n", n);
+    return 0;
+}
+
+uint64_t print_bool(uint64_t n)
+{
+    char *data = n != 0 ? "true" : "false";
+    printf("%s\n", data);
+    return 0;
+}
