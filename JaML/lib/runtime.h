4a6a06259b6d97bd01b07a61540cb8d6d15e8b1f
Maxim Drumov <Drumov.M.A@gmail.com>
2024-03-07 17:39:15 +0300

    Fix closure constructors
    Signed-off-by: Maxim Drumov <Drumov.M.A@gmail.com>

--- JaML/lib/runtime.h
+++ JaML/lib/runtime.h
@@ -9,5 +9,6 @@
 
 uint64_t tuple_make(uint64_t n, ...);
 uint64_t tuple_take(uint64_t tuple, uint64_t n);
+uint64_t apply_n(uint64_t (*func_ptr)(uint64_t, ...), uint64_t n, uint64_t *args);
 uint64_t make_pa(uint64_t f_ptr, uint64_t max_args, uint64_t n, ...);
 uint64_t add_args_to_pa(uint64_t partial_app, uint64_t n, ...);
